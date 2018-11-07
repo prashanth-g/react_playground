a89e52e6882d8eb8e3df9429a15447c6ba71da00
prashanth-g <prashanthcsgce@gmail.com>
2018-11-07 21:33:13 +0530

    Added Components for testing

---
+++ src/Jsx.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+const name = 'Prashanth';
+
+class Jsx extends Component {
+  render() {
+    return (
+      <div className="Jsx">
+          <p>
+            Inside Jsx elements.{name} !
+          </p>
+      </div>
+    );
+  }
+}
+
+export default Jsx;

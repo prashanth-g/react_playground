7b49107f2778e59347e55f57797f9be9686a0851
prashanth-g <prashanthcsgce@gmail.com>
2018-11-08 20:42:11 +0530

    Added Embedding expressions with JSX

--- src/Jsx.js
+++ src/Jsx.js
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 
 const name = 'Prashanth';
 
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+}
+  
+const user = {
+    firstName: 'Prashanth',
+    lastName: 'G'
+};
+
 class Jsx extends Component {
   render() {
     return (
       <div className="Jsx">
           <p>
-            Inside Jsx elements.{name} !
+            Inside Jsx elements.{name}. Full Name: {formatName(user)}!
           </p>
       </div>
     );

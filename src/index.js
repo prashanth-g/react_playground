d48f9b55dedffbc40a658fad11468ac9f4bc9098
prashanth-g <prashanthcsgce@gmail.com>
2018-11-10 21:49:17 +0530

    Added update-rendered-element area

--- src/index.js
+++ src/index.js
@@ -11,4 +11,16 @@ ReactDOM.render(<Jsx />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
+
+function tick() {
+    const element = (
+      <div>
+        <h2>The current time is {new Date().toLocaleTimeString()}.</h2>
+      </div>
+    );
+    ReactDOM.render(element, document.getElementById('root'));
+}
+
+setInterval(tick, 1000);
+
 serviceWorker.unregister();

a54c9c4778ff73d922535700097d50f59cd53fa5
prashanth-g <prashanthcsgce@gmail.com>
2018-11-12 08:59:11 +0530

    Added event handling

---
+++ src/home.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Jsx from './modules/Jsx';
+import Message from './modules/Message';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<Jsx />, document.getElementById('root'));
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+
+function tick() {
+    const element = (
+      <div>
+        <h2>The current time is {new Date().toLocaleTimeString()}.</h2>
+      </div>
+    );
+    ReactDOM.render(element, document.getElementById('root'));
+}
+
+setInterval(tick, 1000);
+
+ReactDOM.render(<Message />, document.getElementById('dev'));
+
+serviceWorker.unregister();
--- src/index.js
+++ src/index.js
@@ -87,5 +87,29 @@ ReactDOM.render(
   document.getElementById('dev03')
 );
 
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn:true};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }));
+  }
+
+  render() {
+    return(
+      <button onClick={this.handleClick}>
+         {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+    );
+  }
+}
+
+ReactDOM.render(<Toggle />, document.getElementById('dev04'));
+
 serviceWorker.unregister();
 

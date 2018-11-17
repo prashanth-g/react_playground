7d98ba3578961c245f036ec625c5e4abc389daae
prashanth-g <prashanthcsgce@gmail.com>
2018-11-17 21:45:20 +0530

    Added Prevent-Component Rendering

--- src/index.js
+++ src/index.js
@@ -215,5 +215,50 @@ ReactDOM.render(
   document.getElementById('dev07')
 )
 
+class WarningBanner extends React.Component {
+
+  render() {
+    
+    if(!this.props.warn) {
+      return null;
+    }
+
+    return(
+      <div>Warning!</div>
+    );
+  }
+}
+
+class Page extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {showWarning:true};
+    this.handleToggleClick = this.handleToggleClick.bind(this);
+  }
+
+  handleToggleClick() {
+    this.setState(state => ({
+      showWarning : !state.showWarning
+    }));
+  }
+
+  render() {
+    return(
+      <div>
+        <WarningBanner warn = {this.state.showWarning} />
+         <button onClick = {this.handleToggleClick} >
+          {this.state.showWarning ? 'Hide' : 'Show'}
+         </button>
+      </div>
+    );
+  }
+  
+}
+
+ReactDOM.render(
+  <Page />,
+  document.getElementById('dev08')
+)
+
 serviceWorker.unregister();
 

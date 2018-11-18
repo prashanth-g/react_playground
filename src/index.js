bc2e58b83311e2326c06e24a97124d763eaecd9a
prashanth-g <prashanthcsgce@gmail.com>
2018-11-18 19:15:14 +0530

    Added form

--- src/index.js
+++ src/index.js
@@ -307,5 +307,40 @@ ReactDOM.render(
   document.getElementById('dev11')
 ) 
 
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value:''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value.toUpperCase()});
+  }
+
+  handleSubmit(event) {
+    alert("Form submitted");
+    event.preventDefault();
+  }
+
+  render() {
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          <input type="text" value={this.state.value} onChange={this.handleChange}/>
+        </label>
+        <input type="submit" value="Submit"/>
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(
+  <NameForm />,
+  document.getElementById('dev12')
+)
+
 serviceWorker.unregister();
 
